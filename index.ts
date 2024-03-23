6898d667913488ba06bd58e0637e672669483ede
misbahshakil233 <141862743+misbahshakil233@users.noreply.github.com>
2024-03-23 14:51:43 +0500

    created gussing numbers

---
+++ index.ts
@@ -0,0 +1,22 @@
+#! /usr/bin/env node
+
+import inquirer from "inquirer";
+
+
+const randomNumber=Math.ceil(Math.random()*10) ;
+
+
+const answers=await inquirer.prompt([
+    {
+        name:"userGuessNumber",
+        type:"number",
+        message:"Please guess number between 1 to 10 : ",
+    }
+]);
+if(answers.userGuessNumber === randomNumber){
+    console.log(" congraulation! you guess correct number")
+   
+}
+else{
+    console.log("you guess wrong number")
+}
