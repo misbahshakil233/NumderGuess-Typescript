501a5a8875ca1addd3031b12a4b74c52a44c6400
misbahshakil233 <141862743+misbahshakil233@users.noreply.github.com>
2024-03-23 15:25:05 +0500

    trying game run contiously help of while loop

--- index.js
+++ index.js
@@ -1,16 +1,29 @@
 #! /usr/bin/env node
 import inquirer from "inquirer";
-const randomNumber = Math.ceil(Math.random() * 10);
-const answers = await inquirer.prompt([
-    {
-        name: "userGuessNumber",
-        type: "number",
-        message: "Please guess number between 1 to 10 : ",
+console.log("Welcome to the number guessing game!");
+async function startGame() {
+    const randomNumber = Math.ceil(Math.random() * 10);
+    let playing = true;
+    while (playing) {
+        const answers = await inquirer.prompt([
+            {
+                name: "userGuessNumber",
+                type: "number",
+                message: "Please guess a number between 1 to 10: "
+            }
+        ]);
+        if (answers.userGuessNumber === randomNumber) {
+            console.log("Congratulations! You guessed the correct number.");
+        }
+        else {
+            console.log("Sorry, you guessed the wrong number.");
+        }
+        const again = await inquirer.prompt({
+            type: "input",
+            name: "restart",
+            message: "Do you want to play again? (y/n): "
+        });
+        playing = (again.restart.toLowerCase() === 'y');
     }
-]);
-if (answers.userGuessNumber === randomNumber) {
-    console.log(" congraulation! you guess correct number");
 }
-else {
-    console.log("you guess wrong number");
-}
+startGame();
